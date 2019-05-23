452b6f93de5c3be31b60bfc3ad08f0ce47063100
FMShelbyGT500 <eightynov@gmail.com>
2019-05-23 21:21:07 +0300

    Develop assistant

---
+++ assistant.py
@@ -0,0 +1,35 @@
+from random import randint
+from pprint import pprint
+import pytest
+
+
+def generate_random_numbers(numbers_count: int, code: str='', digits_count: int=12):
+    need_digits = digits_count - len(code)
+
+    return [int(code + ''.join(map(str, [randint(0, 9) for i in range(need_digits)]))) 
+                for i in range(numbers_count)]
+
+
+def find_numbers(db):
+    template = input('Enter the number (or first few digits):\n')
+    
+    if 'inf' in template:
+        return 'Invalid data'
+
+    try:
+        template = int(template)
+        if template < 0:
+            return 'It can`t be negative'
+    except ValueError:
+        return 'Invalid data'
+
+    return [num for num in db if str(num).startswith(str(template))]
+    
+
+
+def main():
+    test_db = generate_random_numbers(100, '380')
+    print(find_numbers(test_db))
+
+if __name__ == "__main__":
+    main()
